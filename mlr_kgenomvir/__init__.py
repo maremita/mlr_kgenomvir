ce5cade887ff34489410d9e6092cf709eba158ab
Amine M. Remita <remita.amine@courrier.uqam.ca>
2021-09-09 10:36:22 -0300

    update version number

--- mlr_kgenomvir/__init__.py
+++ mlr_kgenomvir/__init__.py
@@ -1,6 +1,6 @@
 #################################
 ##                             ##
-##  2020 (C) Amine M. Remita   ##
+##  2021 (C) Amine M. Remita   ##
 ##                             ##
 ## Laboratoire Bioinformatique ##
 ## Pr. Abdoulaye Diallo Group  ##
@@ -8,8 +8,8 @@
 ##                             ##
 #################################
 
-__author__ = "Amine Remita"
-__date__   = "20200718"
-__version__ = "0.9.3"
+__author__ = ["Amine Remita", "Nicolas De Montigny"]
+__date__   = "20210909"
+__version__ = "0.10.0"
 
 __all__ = ['data', 'models', 'simulation', 'utils']
