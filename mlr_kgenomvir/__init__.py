6132a622fb736238bf622dd40804001d4bd7d7c3
Amine M. Remita <remita.amine@courrier.uqam.ca>
2021-10-12 01:25:48 -0300

    Update setup.py and version 0.10.12

--- mlr_kgenomvir/__init__.py
+++ mlr_kgenomvir/__init__.py
@@ -10,7 +10,7 @@
 #################################
 
 __author__ = ["Amine Remita", "Nicolas De Montigny"]
-__date__   = "20211011"
-__version__ = "0.10.11.2"
+__date__   = "20211012"
+__version__ = "0.10.12"
 
 __all__ = ['data', 'models', 'simulation', 'utils']
